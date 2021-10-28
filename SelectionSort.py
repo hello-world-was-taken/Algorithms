cb5e95a537d6cda3deb2efb081bd9ff47986afc8
hello_world <haileformal@gmail.com>
2021-10-28 10:08:39 +0300

    make the sort more efficient

--- SelectionSort.py
+++ SelectionSort.py
@@ -8,13 +8,14 @@ def selectionSort(arr):
     '''An implementation of selection sort'''
     for i in range(len(arr)):
         minIndex = i # the value that keeps track of the current minimum index
-        for j in range(i, len(arr)): # start comparing from the i'th value to the end of the loop with our swapping but updating the minimum index
+        for j in range(i + 1, len(arr)): # start comparing from the i'th value to the end of the loop with our swapping but updating the minimum index
             if(arr[minIndex] > arr[j]):
                 minIndex = j
         swap(arr, i, minIndex) # finally swap the minimum index with the current i'th index
 
 
-arr = [5, 3, 4, 1, 2]
+# arr = [5, 3, 4, 1, 2]
+arr = [0, 2, 34, 22, 10, 19, 17]
 
 selectionSort(arr)
 
